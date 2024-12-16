b5b57528cb022688a9eb846702ee55a9b53e96e1
WDuur <w.duursma@outlook.com>
2024-12-16 20:06:44 +0100

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}', // Ensure it scans all relevant files
+  ],
+  theme: {
+    extend: {}, // Customize your theme here
+  },
+  plugins: [], // Add plugins here if needed
+}
